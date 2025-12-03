e9c4e050e7c43ad6c88d70a42b3b2189dbce5556
Akhil Jose <akhiljose@gmail.com>
2025-12-03 12:11:10 +0530

    complete day3, built a general solution for n batteries in a bank O (n * m) solution where m is length of each bank

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,10 +1,11 @@
+import java.util.SortedMap
 import kotlin.math.max
 
 fun main() {
-    part1()
+    genericSolution(12)
 }
 
-fun part1() {
+fun part1BF() {
     val banks = readInput("Day03")
     var passcode = 0L
 
@@ -21,6 +22,40 @@ fun part1() {
             }
         }
         passcode += greatest
+    }
+
+    println(passcode)
+}
+
+fun genericSolution(count: Int = 1) {
+    val banks = readInput("Day03")
+    var passcode = 0L
+
+    // SORTED MAPS
+    for (bank in banks) {
+        val sortedMap = emptyMap<Int, Char?>().toSortedMap()
+        val len = bank.length
+        var joltage = 0L
+        repeat(count) {
+            var greaterIdx = 0
+            for (i in 0..<len) {
+                val shouldNull = (sortedMap[i] == null)
+                sortedMap[i] = bank[i]
+                val num = sortedMap.values.joinToString(separator = "", transform = { digit ->
+                    if (digit == null) ""
+                    else "$digit"
+                }).toLong()
+                if (num > joltage) {
+                    greaterIdx = i
+                    joltage = num
+                }
+
+                if (shouldNull)
+                    sortedMap[i] = null
+            }
+            sortedMap[greaterIdx] = bank[greaterIdx]
+        }
+        passcode += joltage
     }
 
     println(passcode)
